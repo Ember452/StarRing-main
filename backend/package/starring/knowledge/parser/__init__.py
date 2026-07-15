dc57b07c215f9934b1e877a2245ece7f24233a13
Solis <16898659+epoch2@user.noreply.gitee.com>
2026-07-15 08:48:08 +0800

    feat(parser): 添加 parser 包 __init__.py

---
+++ backend/package/starring/knowledge/parser/__init__.py
@@ -0,0 +1,27 @@
+﻿from starring.knowledge.parser.base import (
+    BaseDocumentProcessor,
+    DocumentParserException,
+    DocumentProcessorException,
+    OCRException,
+)
+from starring.knowledge.parser.factory import DocumentProcessorFactory
+from starring.knowledge.parser.unified import (
+    SUPPORTED_FILE_EXTENSIONS,
+    MarkdownParseResult,
+    Parser,
+    is_supported_file_extension,
+    parse_source_to_markdown,
+)
+
+__all__ = [
+    "BaseDocumentProcessor",
+    "DocumentProcessorException",
+    "DocumentParserException",
+    "OCRException",
+    "DocumentProcessorFactory",
+    "MarkdownParseResult",
+    "Parser",
+    "SUPPORTED_FILE_EXTENSIONS",
+    "is_supported_file_extension",
+    "parse_source_to_markdown",
+]
